0b8ff200d90841941c3cd5ad9aa8f09a39a86815
AnneLindberg <aans@live.dk>
2020-03-24 10:26:46 +0100

    added id to button chatview.fxml and the attributes in the controller

--- src/view/chatView/ChatViewController.java
+++ src/view/chatView/ChatViewController.java
@@ -2,16 +2,25 @@ package view.chatView;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import networking.client.EchoClient;
 
 public class ChatViewController
 {
+  @FXML private Button buttonSend;
   @FXML private TextArea idOutputMessage;
   @FXML private TextField idInputMessage;
 
+
+  public ChatViewController()
+  {
+  }
+
   public void OnSendMessage(ActionEvent actionEvent)
   {
+
   }
 
   public void OnLogOut(ActionEvent actionEvent)
